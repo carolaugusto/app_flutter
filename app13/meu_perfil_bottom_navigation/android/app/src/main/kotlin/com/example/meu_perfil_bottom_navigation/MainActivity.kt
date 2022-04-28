c6d5244e352f3e076877a78dd89d1462452129f3
Ana Carolina Augusto <rm87820@fiap.com.br>
2022-04-28 00:43:06 -0300

    Meu Perfil com Bottom Navigation

---
+++ app13/meu_perfil_bottom_navigation/android/app/src/main/kotlin/com/example/meu_perfil_bottom_navigation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.meu_perfil_bottom_navigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app13/meu_perfil_bottom_navigation/lib/pages/experiencia.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Experiencia extends StatelessWidget {
+  const Experiencia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Experiência'),
+      ),
+    );
+  }
+}
